429021c2058517e7449a2da7d7d4ccfb1df4a34b
JOBY TO JOSEPH <jobytom@gmail.com>
2025-04-07 23:05:46 +0530

    Update top_rating_view.sql

--- top_rating_view.sql
+++ top_rating_view.sql
@@ -1,1 +1,14 @@
+-- Create a view for the top 5 highest-rated restaurants
+CREATE VIEW TopRatingRestaurants AS
+SELECT 
+    RestaurantName,
+    RestaurantType,
+    Rating,
+    Area
+FROM jomato_dataset
+WHERE Rating IS NOT NULL
+ORDER BY Rating DESC
+LIMIT 5;
 
+-- Query the view
+SELECT * FROM TopRatingRestaurants;
